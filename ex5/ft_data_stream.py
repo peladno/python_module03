5833683b418d55d390b0b41ae2a4561a6ba22be8
peladno <j.perezurrutia@gmail.com>
2026-06-26 21:47:37 +0900

    ex5 done

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -1,16 +1,36 @@
 import random
 from typing import Generator
 
+PLAYERS = ["alice", "bob", "charlie", "dylan"]
+ACTIONS = ["run", "eat", "sleep", "grab", "move", "climb", "swim", "release"]
+
 
 def gen_event() -> Generator[tuple[str, str], None, None]:
-    players = ["alice", "bob", "charlie", "dylan"]
-    actions = ["run", "eat", "sleep", "grab",
-               "move", "climb", "swim", "use", "release"]
     while True:
-        yield (random.choice(players), random.choice(actions))
+        yield (random.choice(PLAYERS), random.choice(ACTIONS))
 
 
-g = gen_event()
-for i in range(1000):
-    name, action = next(g)
-    print(f"Event {i}: Player {name} did action {action}")
+def remove_events(events: list[tuple[str, str]]) -> Generator[tuple[str, str], None, None]:
+    while len(events) > 0:
+        choosed = random.choice(events)
+        events.remove(choosed)
+        yield choosed
+
+
+def main() -> None:
+    g = gen_event()
+
+    for i in range(1000):
+        name, action = next(g)
+        print(f"Event {i}: Player {name} did action {action}")
+
+    data: list[tuple[str, str]] = [next(g) for _ in range(10)]
+    print(f"Built list of 10 events: ", data)
+
+    for e in remove_events(data):
+        print("Got event from list: ", e)
+        print("Remains in list; ", data)
+
+
+if __name__ == "__main__":
+    main()
