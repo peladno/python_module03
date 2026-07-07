a73c31fa77150dc71cbb5d5838b2c6442b96afb8
peladno <j.perezurrutia@gmail.com>
2026-07-07 20:41:08 +0900

    flake8 fixed

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -5,12 +5,12 @@ PLAYERS = ["alice", "bob", "charlie", "dylan"]
 ACTIONS = ["run", "eat", "sleep", "grab", "move", "climb", "swim", "release"]
 
 
-def gen_event() -> Generator[tuple[str, str], None, None]:
+def gen_event() -> Generator[tuple[str, str]]:
     while True:
         yield (random.choice(PLAYERS), random.choice(ACTIONS))
 
 
-def remove_events(events: list[tuple[str, str]]) -> Generator[tuple[str, str], None, None]:
+def rm_events(events: list[tuple[str, str]]) -> Generator[tuple[str, str]]:
     while len(events) > 0:
         choosed = random.choice(events)
         events.remove(choosed)
@@ -25,9 +25,9 @@ def main() -> None:
         print(f"Event {i}: Player {name} did action {action}")
 
     data: list[tuple[str, str]] = [next(g) for _ in range(10)]
-    print(f"Built list of 10 events: ", data)
+    print("Built list of 10 events: ", data)
 
-    for e in remove_events(data):
+    for e in rm_events(data):
         print("Got event from list: ", e)
         print("Remains in list; ", data)
 
