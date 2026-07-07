34382a4a2c35c27d6a1338bc1cc503be0d8ca1f1
peladno <j.perezurrutia@gmail.com>
2026-07-07 20:51:51 +0900

    done

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,15 +1,22 @@
 import sys
 
-print("=== Command Quest ===")
-path, *args = sys.argv
-argc = len(sys.argv)
-print(f"Program name: {path}")
-if argc == 1:
-    print("No argument provided!")
-else:
-    print(f"Arguments received {argc - 1}")
-i = 0
-for data in args:
-    print(f"Argument {i + 1}: {data}")
-    i += 1
-print(f"Total arguments: {argc}")
+
+def main() -> None:
+    print("=== Command Quest ===")
+
+    path, *args = sys.argv
+    argc = len(sys.argv)
+    print(f"Program name: {path}")
+    if argc == 1:
+        print("No argument provided!")
+    else:
+        print(f"Arguments received {argc - 1}")
+    i = 0
+    for data in args:
+        print(f"Argument {i + 1}: {data}")
+        i += 1
+    print(f"Total arguments: {argc}")
+
+
+if __name__ == "__main__":
+    main()
